0bb1f9f640074aa72d73fff6448e34da7ce4d7d4
Helder Correia <helder.pereira.correia@gmail.com>
2008-11-26 00:37:59 +0100

    Renamed i18n to locale.

--- make.sh
+++ make.sh
@@ -34,7 +34,7 @@ MSM="msgmerge -U --quiet"
 # extract marked strings and create POT                                        #
 #------------------------------------------------------------------------------#
 
-$T2P -o i18n/speedcrunch.org.pot templates/*.html
+$T2P -o locale/speedcrunch.org.pot templates/*.html
 
 #------------------------------------------------------------------------------#
 # merge and generate pages                                                     #
@@ -44,14 +44,14 @@ LANGS="de_DE en_US es_ES fr_FR it_IT nl_NL pt_BR pt_PT"
 
 for lang in $LANGS
 do
-	touch i18n/speedcrunch.org.$lang.po
+	touch locale/speedcrunch.org.$lang.po
 	mkdir -p $DOM/$lang
-	$MSM i18n/speedcrunch.org.$lang.po i18n/speedcrunch.org.pot
-	$P2H -t templates/index.html       -i i18n/$DOM.$lang.po -o $DOM/$lang/index.html
-	$P2H -t templates/features.html    -i i18n/$DOM.$lang.po -o $DOM/$lang/features.html
-	$P2H -t templates/screenshots.html -i i18n/$DOM.$lang.po -o $DOM/$lang/screenshots.html
-	$P2H -t templates/faq.html         -i i18n/$DOM.$lang.po -o $DOM/$lang/faq.html
-	$P2H -t templates/download.html    -i i18n/$DOM.$lang.po -o $DOM/$lang/download.html
+	$MSM locale/speedcrunch.org.$lang.po locale/speedcrunch.org.pot
+	$P2H -t templates/index.html       -i locale/$DOM.$lang.po -o $DOM/$lang/index.html
+	$P2H -t templates/features.html    -i locale/$DOM.$lang.po -o $DOM/$lang/features.html
+	$P2H -t templates/screenshots.html -i locale/$DOM.$lang.po -o $DOM/$lang/screenshots.html
+	$P2H -t templates/faq.html         -i locale/$DOM.$lang.po -o $DOM/$lang/faq.html
+	$P2H -t templates/download.html    -i locale/$DOM.$lang.po -o $DOM/$lang/download.html
 done
 
 #------------------------------------------------------------------------------#
